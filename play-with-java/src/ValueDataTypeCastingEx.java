ef79c35be068363301962c4c7243041de4224b0a
nag <nagtraininglab@gmail.com>
2023-11-01 17:39:03 +0530

    value type casting

---
+++ play-with-java/src/ValueDataTypeCastingEx.java
@@ -0,0 +1,15 @@
+public class ValueDataTypeCastingEx {
+    public static void main(String[] args) {
+
+        int x=6;
+        int y=4;
+        float z= (float)x/y;
+        System.out.println(z);
+
+
+        int v=456;
+        byte b=(byte)v; // byte : -128 to 127
+        System.out.println(b);
+
+    }
+}
