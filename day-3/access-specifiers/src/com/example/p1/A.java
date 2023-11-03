b590749042f94202b125b57f074414ef419800ca
nag <nagtraininglab@gmail.com>
2023-11-03 17:28:18 +0530

    with notes

---
+++ day-3/access-specifiers/src/com/example/p1/A.java
@@ -0,0 +1,19 @@
+package p1;
+
+public class A {
+
+    // variables
+    private int pri=1;
+    int de=2;
+    protected int pro=3;
+    public int pub=4;
+    // constructor
+    // methods
+    public void aObjMethod(){
+        System.out.println(pri);
+        System.out.println(de);
+        System.out.println(pro);
+        System.out.println(pub);
+    }
+
+}
