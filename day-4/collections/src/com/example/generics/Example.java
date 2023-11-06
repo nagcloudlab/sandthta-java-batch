5f7786010a4b688e7dae54c92e5d35efa7b3f5fe
Nagabhushanam <nagabhushanam.n@revature.com>
2023-11-06 19:00:06 +0530

    day-4 commit

---
+++ day-4/collections/src/com/example/generics/Example.java
@@ -0,0 +1,31 @@
+package com.example.generics;
+
+// generics => java 1.5
+
+// box is data-structure with array
+class Box<E> {
+    Object[] data = new Object[10];
+    int index = -1;
+
+    public void add(E s) {
+        index++;
+        data[index] = s;
+    }
+
+    public E get(int i) {
+        return (E) data[i];
+    }
+}
+
+public class Example {
+    public static void main(String[] args) {
+
+        Box<String> box1 = new Box<>(); // with generic class => can create type-safety object
+        box1.add("string");
+        //box1.add(123);
+
+        Box<Integer> box2 = new Box<>(); // with generic class => can create type
+        box2.add(123);
+
+    }
+}
