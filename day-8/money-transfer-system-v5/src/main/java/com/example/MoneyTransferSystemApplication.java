c78eb987833d58a58dd7aa91435970f84ac12be0
Nagabhushanam <nagabhushanam.n@revature.com>
2023-11-17 18:53:25 +0530

    day-8 commit

--- day-8/money-transfer-system-v5/src/main/java/com/example/MoneyTransferSystemApplication.java
+++ day-8/money-transfer-system-v5/src/main/java/com/example/MoneyTransferSystemApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("bank.properties")
 // or
 //@Configuration
 //@EnableAutoConfiguration
--- day-8/money-transfer-system-v5/src/main/java/com/example/service/UPITransferService.java
+++ day-8/money-transfer-system-v5/src/main/java/com/example/service/UPITransferService.java
@@ -8,6 +8,7 @@ import com.example.exception.AccountNotFoundException;
 import com.example.repository.AccountRepository;
 import com.example.repository.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,9 +24,16 @@ public class UPITransferService implements TransferService {
     @Autowired
     private TransactionRepository transactionRepository;
 
+    @Value("${mybank.upi.transfer.limit: 1000.00}")
+    private double transferLimit;
+
     @Override
     @Transactional
     public TransferStatus transfer(double amount, String sourceAccNumber, String destAccNumber) {
+
+        System.out.println("-".repeat(50));
+        System.out.println(transferLimit);
+        System.out.println("-".repeat(50));
 
         // load source & destination accounts
         Account sourceAccount = accountRepository.findById(sourceAccNumber).orElseThrow(
